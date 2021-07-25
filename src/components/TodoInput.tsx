28156710e15299272450e276eeca0ca3b540d8fc
Guilherme_Nome_Commit <guilherme_nome_commit@gmail.com>
2021-07-24 22:58:47 -0300

    Update TodoInput.tsx
    Adicionado a função handleAddNewTask que adiciona uma nova task e limpa o input

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -3,31 +3,38 @@ import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 interface TodoInputProps {
-  addTask: (task: string) => void;
+  addTask: (task: string) => void;  
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
   function handleAddNewTask() {
+    if (!task.trim) {
+      addTask(task)
+      setTask('')
+    }
     //TODO - Call addTask if task not empty and clean input value 
   }
 
   return (
     <View style={styles.inputContainer}>
       <TextInput 
-        style={styles.input} 
+        style={styles.input}
+        value={task} 
         placeholder="Adicionar novo todo..."
         placeholderTextColor="#B2B2B2"
         returnKeyType="send"
         selectionColor="#666666"
+        onChangeText={setTask}
+        onSubmitEditing={handleAddNewTask}
         //TODO - use value, onChangeText and onSubmitEditing props
       />
       <TouchableOpacity
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
-        //TODO - onPress prop
+        onPress={handleAddNewTask}
       >
         <Icon name="chevron-right" size={24} color="#B2B2B2" />
       </TouchableOpacity>
